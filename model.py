cb5663ad2f009f468768215bdd63c680cbb37b1c
benny <bennycortese@gmail.com>
2024-02-25 20:16:24 -0500

    Changed model repo to work better with Mixtral for our ML pipeline

--- model.py
+++ model.py
@@ -1,31 +1,124 @@
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
-# import deepspeed
+import os
+import time
 
-model_name = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+from modal import Image, Stub, enter, gpu, method
 
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
-# model = AutoModelForCausalLM.from_pretrained(model_name)
+MODEL_DIR = "/model"
+BASE_MODEL = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+GPU_CONFIG = gpu.A100(memory=80, count=2)
 
-# model.save_pretrained('../../../blue/kapooramanpreet/benjamincortese/mixtral_model')
-# tokenizer.save_pretrained('../../../blue/kapooramanpreet/benjamincortese/mixtral_tokenizer')
 
-device_map = {
-    "cuda:0": list(range(0, 13)),
-    "cuda:1": list(range(13, 26)),
-    "cpu": list(range(26, 39))
-}
+def download_model_to_folder():
+    from huggingface_hub import snapshot_download
+    from transformers.utils import move_cache
 
-model = AutoModelForCausalLM.from_pretrained('../../../blue/kapooramanpreet/benjamincortese/mixtral_model', device_map='auto')
-tokenizer = AutoTokenizer.from_pretrained('../../../blue/kapooramanpreet/benjamincortese/mixtral_tokenizer', device_map='auto')
+    os.makedirs(MODEL_DIR, exist_ok=True)
 
-# model.encoder.to('cuda:0')
-# model.decoder.to('cudo:1')
+    snapshot_download(
+        BASE_MODEL,
+        local_dir=MODEL_DIR,
+        ignore_patterns="*.pt",  # Using safetensors
+    )
+    move_cache()
 
-# model.parallelize(device_map)
+vllm_image = (
+    Image.from_registry(
+        "nvidia/cuda:12.1.0-base-ubuntu22.04", add_python="3.10"
+    )
+    .pip_install(
+        "vllm==0.2.5",
+        "huggingface_hub==0.19.4",
+        "hf-transfer==0.1.4",
+        "torch==2.1.2",
+    )
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
+    .run_function(download_model_to_folder, timeout=60 * 20)
+)
 
-text = "What do you eat with Bananas?"
-inputs = tokenizer(text, return_tensors="pt")
+stub = Stub("example-vllm-mixtral")
 
-outputs = model.generate(**inputs, max_new_tokens=120)
-print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+@stub.cls(
+    gpu=GPU_CONFIG,
+    timeout=60 * 10,
+    container_idle_timeout=60 * 10,
+    allow_concurrent_inputs=10,
+    image=vllm_image,
+)
+class Model:
+    @enter()
+    def start_engine(self):
+        from vllm.engine.arg_utils import AsyncEngineArgs
+        from vllm.engine.async_llm_engine import AsyncLLMEngine
+
+        if GPU_CONFIG.count > 1:
+            # Patch issue from https://github.com/vllm-project/vllm/issues/1116
+            import ray
+
+            ray.shutdown()
+            ray.init(num_gpus=GPU_CONFIG.count)
+
+        engine_args = AsyncEngineArgs(
+            model=MODEL_DIR,
+            tensor_parallel_size=GPU_CONFIG.count,
+            gpu_memory_utilization=0.90,
+        )
+
+        self.engine = AsyncLLMEngine.from_engine_args(engine_args)
+        self.template = "<s> [INST] {user} [/INST] "
+
+        # Performance improvement from https://github.com/vllm-project/vllm/issues/2073#issuecomment-1853422529
+        if GPU_CONFIG.count > 1:
+            import subprocess
+
+            RAY_CORE_PIN_OVERRIDE = "cpuid=0 ; for pid in $(ps xo '%p %c' | grep ray:: | awk '{print $1;}') ; do taskset -cp $cpuid $pid ; cpuid=$(($cpuid + 1)) ; done"
+            subprocess.call(RAY_CORE_PIN_OVERRIDE, shell=True)
+
+    @method()
+    async def completion_stream(self, user_question):
+        from vllm import SamplingParams
+        from vllm.utils import random_uuid
+
+        sampling_params = SamplingParams(
+            temperature=0.75,
+            max_tokens=1024,
+            repetition_penalty=1.1,
+        )
+
+        t0 = time.time()
+        request_id = random_uuid()
+        result_generator = self.engine.generate(
+            self.template.format(user=user_question),
+            sampling_params,
+            request_id,
+        )
+        index, num_tokens = 0, 0
+        async for output in result_generator:
+            if (
+                output.outputs[0].text
+                and "\ufffd" == output.outputs[0].text[-1]
+            ):
+                continue
+            text_delta = output.outputs[0].text[index:]
+            index = len(output.outputs[0].text)
+            num_tokens = len(output.outputs[0].token_ids)
+
+            yield text_delta
+
+        print(f"Generated {num_tokens} tokens in {time.time() - t0:.2f}s")
+
+@stub.local_entrypoint()
+def main():
+    model = Model()
+    questions = [
+        "Implement a Python function to compute the Fibonacci numbers.",
+        "What is the fable involving a fox and grapes?",
+        "What were the major contributing factors to the fall of the Roman Empire?",
+        "Describe the city of the future, considering advances in technology, environmental changes, and societal shifts.",
+        "What is the product of 9 and 8?",
+        "Who was Emperor Norton I, and what was his significance in San Francisco's history?",
+    ]
+    for question in questions:
+        print("Sending new request:", question)
+        for text in model.completion_stream.remote_gen(question):
+            print(text, end="", flush=True)
+
