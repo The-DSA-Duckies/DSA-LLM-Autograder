e93f8f4dcea6f921e5d00524f1d427e611b7ee36
benny <bennycortese@gmail.com>
2024-02-26 04:43:03 -0500

    Added new infra, have to use GPT-3.5 turbo for cost, and modal for fast development. Have an example student report grading!

--- model.py
+++ model.py
@@ -1,124 +1,79 @@
 import os
 import time
 
-from modal import Image, Stub, enter, gpu, method
+import modal
 
-MODEL_DIR = "/model"
-BASE_MODEL = "mistralai/Mixtral-8x7B-Instruct-v0.1"
-GPU_CONFIG = gpu.A100(memory=80, count=2)
+from modal import Image, Stub, wsgi_app
 
+bot_image = modal.Image.debian_slim().pip_install("openai")
+bot_image = bot_image.pip_install("numpy")
+bot_image = bot_image.pip_install("pandas")
+bot_image = bot_image.pip_install("youtube_transcript_api")
+bot_image = bot_image.pip_install("flask")
+bot_image = bot_image.pip_install("flask_cors")
 
-def download_model_to_folder():
-    from huggingface_hub import snapshot_download
-    from transformers.utils import move_cache
+stub = modal.Stub("GPT wins 😔", image=bot_image)
 
-    os.makedirs(MODEL_DIR, exist_ok=True)
-
-    snapshot_download(
-        BASE_MODEL,
-        local_dir=MODEL_DIR,
-        ignore_patterns="*.pt",  # Using safetensors
+@stub.function(secret=modal.Secret.from_name("my-openai-secret"))
+def complete_text(prompt):
+    from openai import OpenAI
+    client = OpenAI()
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125", # note, test this with gpt-4 for quality improvements
+        messages=[
+            {"role": "system", "content": "You are an instructor."},
+            {"role": "user", "content": prompt},
+        ]
     )
-    move_cache()
-
-vllm_image = (
-    Image.from_registry(
-        "nvidia/cuda:12.1.0-base-ubuntu22.04", add_python="3.10"
-    )
-    .pip_install(
-        "vllm==0.2.5",
-        "huggingface_hub==0.19.4",
-        "hf-transfer==0.1.4",
-        "torch==2.1.2",
-    )
-    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})
-    .run_function(download_model_to_folder, timeout=60 * 20)
-)
-
-stub = Stub("example-vllm-mixtral")
-
-@stub.cls(
-    gpu=GPU_CONFIG,
-    timeout=60 * 10,
-    container_idle_timeout=60 * 10,
-    allow_concurrent_inputs=10,
-    image=vllm_image,
-)
-class Model:
-    @enter()
-    def start_engine(self):
-        from vllm.engine.arg_utils import AsyncEngineArgs
-        from vllm.engine.async_llm_engine import AsyncLLMEngine
-
-        if GPU_CONFIG.count > 1:
-            # Patch issue from https://github.com/vllm-project/vllm/issues/1116
-            import ray
-
-            ray.shutdown()
-            ray.init(num_gpus=GPU_CONFIG.count)
-
-        engine_args = AsyncEngineArgs(
-            model=MODEL_DIR,
-            tensor_parallel_size=GPU_CONFIG.count,
-            gpu_memory_utilization=0.90,
-        )
-
-        self.engine = AsyncLLMEngine.from_engine_args(engine_args)
-        self.template = "<s> [INST] {user} [/INST] "
-
-        # Performance improvement from https://github.com/vllm-project/vllm/issues/2073#issuecomment-1853422529
-        if GPU_CONFIG.count > 1:
-            import subprocess
-
-            RAY_CORE_PIN_OVERRIDE = "cpuid=0 ; for pid in $(ps xo '%p %c' | grep ray:: | awk '{print $1;}') ; do taskset -cp $cpuid $pid ; cpuid=$(($cpuid + 1)) ; done"
-            subprocess.call(RAY_CORE_PIN_OVERRIDE, shell=True)
-
-    @method()
-    async def completion_stream(self, user_question):
-        from vllm import SamplingParams
-        from vllm.utils import random_uuid
-
-        sampling_params = SamplingParams(
-            temperature=0.75,
-            max_tokens=1024,
-            repetition_penalty=1.1,
-        )
-
-        t0 = time.time()
-        request_id = random_uuid()
-        result_generator = self.engine.generate(
-            self.template.format(user=user_question),
-            sampling_params,
-            request_id,
-        )
-        index, num_tokens = 0, 0
-        async for output in result_generator:
-            if (
-                output.outputs[0].text
-                and "\ufffd" == output.outputs[0].text[-1]
-            ):
-                continue
-            text_delta = output.outputs[0].text[index:]
-            index = len(output.outputs[0].text)
-            num_tokens = len(output.outputs[0].token_ids)
-
-            yield text_delta
-
-        print(f"Generated {num_tokens} tokens in {time.time() - t0:.2f}s")
+    return response.choices[0].message.content
 
 @stub.local_entrypoint()
 def main():
-    model = Model()
+    assignment = open("assignment.txt", "r").read()
+    rubric = open("rubric.txt", "r").read()
+    student_submission = open("student.txt", "r", encoding='utf-8').read()
+    example = open("example.txt", "r", encoding='utf-8').read()
+
     questions = [
-        "Implement a Python function to compute the Fibonacci numbers.",
-        "What is the fable involving a fox and grapes?",
-        "What were the major contributing factors to the fall of the Roman Empire?",
-        "Describe the city of the future, considering advances in technology, environmental changes, and societal shifts.",
-        "What is the product of 9 and 8?",
-        "Who was Emperor Norton I, and what was his significance in San Francisco's history?",
+        f"""
+        Act as a very dilligent teacher's assistant grading a given assignment from the rubric provided. Explain for each decision you make why you
+        made it, and why it checks off certain boxes on the rubric. 
+
+        Do the grading step by step by going through each part of the ASSIGNMENT and STUDENT SUBMISSION. Then compare that answer to the grading criteria. Use the EXAMPLE FEEDBACK for formatting, changing the w, x, y and z variables to the correct numbers. The answer should be formatted as follows in a single text block:
+
+        CRITERIA: [go through each criteria and explain if the student has completed it]
+
+        FEEDBACK: [what did the student do well and what could be improved (but isn't wrong) in plain text]
+
+        FIXING: [Anything that is missing, incorrect or incomplete from the ASSIGNMENT]
+
+        GRADE: [Each breakdown of points lost for missing parts of the rubric]
+
+        Here follows the assignment, grading criteria and student submission
+
+        ASSIGNMENT:
+        \"\"\"
+        {assignment}
+        \"\"\"
+
+        GRADING:
+        \"\"\"
+        {rubric}
+        \"\"\"
+
+        STUDENT SUBMISSION:
+        \"\"\"
+        {student_submission}
+        \"\"\"
+
+        EXAMPLE FEEDBACK:
+        \"\"\"
+        {example}    
+        \"\"\"
+    """
     ]
     for question in questions:
         print("Sending new request:", question)
-        for text in model.completion_stream.remote_gen(question):
-            print(text, end="", flush=True)
+        print(complete_text.remote(question))
+            
 
