4ccbeffc97f9c690b1c8fc15d3fe2da66f25d278
benny <bennycortese@gmail.com>
2024-03-07 01:39:15 -0500

    Added anthropic api calls/setup to the repo for easy use if it ends up being better than gpt

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ bot_image = bot_image.pip_install("flask_cors")
 stub = modal.Stub("GPT wins 😔", image=bot_image)
 
 @stub.function(secret=modal.Secret.from_name("my-openai-secret"))
-def complete_text(prompt):
+def complete_text_gpt(prompt):
     from openai import OpenAI
     client = OpenAI()
     response = client.chat.completions.create(
@@ -26,6 +26,30 @@ def complete_text(prompt):
         ]
     )
     return response.choices[0].message.content
+
+
+@stub.function(secret=modal.Secret.from_name("my-openai-secret"))
+def complete_text_anthropic(prompt):
+    import anthropic
+
+    client = anthropic.Anthropic(
+        # defaults to os.environ.get("ANTHROPIC_API_KEY")
+        api_key="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",
+    )
+
+    message = client.messages.create(
+        model="claude-3-opus-20240229",
+        max_tokens=32000,
+        temperature=0.0,
+        system="Respond only in Yoda-speak.",
+        messages=[
+            {"role": "system", "content": "You are an instructor who is very good at grading."},
+            {"role": "user", "content": prompt},
+        ]
+    )
+
+    return message.content
+
 
 @stub.local_entrypoint()
 def main():
@@ -81,6 +105,6 @@ def main():
     ]
     for question in questions:
         print("Sending new request:", question)
-        print(complete_text.remote(question))
+        print(complete_text_anthropic.remote(question))
             
 
